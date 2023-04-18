06875ec48cab73703515451f27d0af0b76493034
pdp <john.lgd65@gmail.com>
2023-04-18 08:20:38 +0500

    initial

---
+++ src/main/java/uz/pdp/springadvanced/SpringadvancedApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.springadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringadvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringadvancedApplication.class, args);
+    }
+
+}
