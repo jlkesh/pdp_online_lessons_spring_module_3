96aafd98ca14dd1b5ab9b026303ed6356978585c
pdp <john.lgd65@gmail.com>
2023-04-19 08:54:07 +0500

    call_api_webclient examples done :)

--- src/main/java/uz/pdp/springadvanced/SpringadvancedApplication.java
+++ src/main/java/uz/pdp/springadvanced/SpringadvancedApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Duration;
 
@@ -23,4 +24,8 @@ public class SpringadvancedApplication {
                 .build();
     }
 
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
 }
--- src/main/java/uz/pdp/springadvanced/resource/CommentResource.java
+++ src/main/java/uz/pdp/springadvanced/resource/CommentResource.java
@@ -5,21 +5,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import uz.pdp.springadvanced.post.dto.CommentCreateDTO;
 import uz.pdp.springadvanced.post.dto.CommentDTO;
 
-import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CommentResource {
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
     @Value("${comments.url.postComments}")
     private String postCommentsURL;
@@ -29,31 +30,20 @@ public class CommentResource {
 
 
     public List<CommentDTO> getAllComments(@NonNull Integer postID) {
-        // ResponseEntity<List> responseEntity = restTemplate.getForEntity(url, List.class, post.getId());
-        // List comments = responseEntity.getBody();
-        // List comments = restTemplate.getForObject(url, List.class, post.getId());
-        // HttpHeaders headers = new HttpHeaders();
-        // headers.add("Authorization", "Bearer token................");
-        // headers.setBearerAuth("token");
-        // headers.setBasicAuth("username","123");
-        // headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
-        // HttpEntity<Object> httpEntity = new HttpEntity<>(new Object(), headers);
-        try {
-            return restTemplate.exchange(
-                    postCommentsURL,
-                    HttpMethod.GET,
-                    HttpEntity.EMPTY,
-                    new ParameterizedTypeReference<List<CommentDTO>>() {
-                    },
-                    postID).getBody();
-        } catch (ResourceAccessException e) {
-            e.printStackTrace();
-        }
-        return Collections.emptyList();
+        return webClient
+                .get()
+                .uri(postCommentsURL, postID)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<CommentDTO>>() {})
+                .block();
     }
 
     public void saveAll(List<CommentCreateDTO> commentCreateDTOS) {
-        HttpEntity<List<CommentCreateDTO>> httpEntity = new HttpEntity<>(commentCreateDTOS);
-        restTemplate.exchange(saveCommentsURL, HttpMethod.POST, httpEntity, Void.class);
+        webClient.post()
+                .uri(saveCommentsURL)
+                .body(BodyInserters.fromValue(commentCreateDTOS))
+                .retrieve()
+                .bodyToMono(Void.class)
+                .block();
     }
 }
